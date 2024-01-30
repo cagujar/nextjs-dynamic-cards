5d6842affaa10f2322212b8b2133f0fd80c0a8c7
Chrissa Agujar <55868590+cagujar@users.noreply.github.com>
2024-01-31 00:53:05 +0800

    mapping the cards

--- app/page.js
+++ app/page.js
@@ -1,10 +1,23 @@
 import Image from "next/image";
+import cardList from './data';
 
 export default function Home() {
   return (
     <div>
-      <main className="container py-36 mx-auto px-8">
-        hello world
+      <main className="container py-36 mx-auto px-8 bg-slate-100 text-gray-900">
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-6">
+          {cardList.map(card => (
+            <div className="shadow-lg">
+              <img src={card.img} alt="" className="rounded-t-lg" />
+              <div className="p-5">
+                <h3 className="text-3xl font-bold mb-3">{card.title}</h3>
+                <p>{card.text}</p>
+              </div>
+             
+            </div>
+          ))}
+        </div>
+        
       </main>
     </div>
   );
